6c30a8a46e9b621d7c897f2f1bc8ff6a0ed0e634
TimothyKBook <TimothyKBook@gmail.com>
2017-01-15 01:34:20 -0500

    Roxygenized.

--- R/catcorr.R
+++ R/catcorr.R
@@ -1,7 +1,25 @@
+#' catCorr
+#'
+#' Produce metrics of correlation among categorical variables.
+#'
+#' @param x Either a vector or data.frame to analyze.
+#' @param y If x is a vector, the `y` is the vector to compare it to.
+#' @param bias_correction Logical, if TRUE (default), use the bias correction in
+#' Cramer's V
+#' @param type Which statistics to produce.  Options are 'tschuprow', 'tschu',
+#' 't' for Tschuprow's T statistic, 'cramer', 'c', 'v' for Cramer's V statistic
+#' and p-values, and 'both' or 'b' for both options.
+#'
+#' @return Cramer's V (\code{cramer_V}), Tschuprow's T (\code{tschu_T}), and/or
+#' p-value(s) (\code{pval}).
+#' @export
 catCorr <- function(x = NULL, y = NULL,
                     bias_correction = TRUE,
                     type = "both")
 {
+
+  # TODO: Vectorize matrix computation
+  # TODO: Supress chi-sq warnings
   if (length(x) > 0 & !is.null(y))
   {
     output <- find_corr_vec(x, y, type = type, bias_correction = bias_correction)
